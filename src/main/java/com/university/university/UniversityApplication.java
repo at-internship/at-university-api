6683f558a9a7c3162db7f140ab0b421f58020490
JoeDyenz <joedyenz@gmail.com>
2020-09-18 17:39:50 -0500

    Commit changed structure names and controller, corrected syntax

---
+++ src/main/java/com/university/university/UniversityApplication.java
@@ -0,0 +1,13 @@
+package com.university.university;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversityApplication.class, args);
+	}
+
+}
