f5225865f0d6e9567cd272267dfb8defdf4e6e51
JoeDyenz <joedyenz@gmail.com>
2020-09-18 17:34:11 -0500

    Commit changed structure names and controller

---
+++ src/test/java/com/university/university/UniversityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.university.university;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
