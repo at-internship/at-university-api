634e1670884b792c376165d92f97ef7967e137f6
ErnestoZamora <71523556+ErnestoZamora@users.noreply.github.com>
2020-09-18 16:37:06 -0500

    Added files to start project

---
+++ src/main/java/com/heroku/heroku/HerokuApplication.java
@@ -0,0 +1,13 @@
+package com.heroku.heroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heroku/heroku/HerokuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heroku.heroku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerokuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
