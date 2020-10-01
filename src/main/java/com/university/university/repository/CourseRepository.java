1f0d11a96990fccd2fee949741c2b7c00cde473a
JoeDyenz <joedyenz@gmail.com>
2020-10-01 14:53:27 -0500

    US 009

---
+++ src/main/java/com/university/university/repository/CourseRepository.java
@@ -0,0 +1,14 @@
+package com.university.university.repository;
+
+import java.util.List;
+
+import com.university.university.model.Course;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends MongoRepository<Course, String> {
+
+
+
+}
