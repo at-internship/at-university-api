988ecce770ab2d39e69fa3d5c952d6f470f6b8ac
VianeyG <vianey.garcia@agilethought.com>
2020-10-02 10:04:30 -0500

    Implement Swagger on at-university-api

--- src/main/java/com/university/university/controller/UniversityController.java
+++ src/main/java/com/university/university/controller/UniversityController.java
@@ -1,10 +1,8 @@
 package com.university.university.controller;
 
+
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import lombok.extern.slf4j.Slf4j;
 
