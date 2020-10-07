8d88e7581fe5d3920bc867427eedcb4e1cb0e681
JoeDyenz <joedyenz@gmail.com>
2020-10-07 09:39:08 -0500

    At-University-Api-009: domain for category update

--- src/main/java/com/agilethought/internship/university/model/Course.java
+++ src/main/java/com/agilethought/internship/university/model/Course.java
@@ -3,7 +3,6 @@ package com.agilethought.internship.university.model;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
--- src/main/java/com/agilethought/internship/university/service/impl/UniversityServiceImpl.java
+++ src/main/java/com/agilethought/internship/university/service/impl/UniversityServiceImpl.java
@@ -6,10 +6,12 @@ import com.agilethought.internship.university.model.Course;
 import com.agilethought.internship.university.repository.CoursesRepository;
 import com.agilethought.internship.university.service.UniversityService;
 import com.agilethought.internship.university.service.common.CategoryConstants;
+import com.agilethought.internship.university.domain.CourseResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -43,9 +45,31 @@ public class UniversityServiceImpl implements UniversityService {
     }
 
 
-    public List<Course> getCourses() {
+    public List<CourseResponse> getCourses() {
 
-        return repository.findAll();
+        List<Course> unchanged = repository.findAll();
+        List<CourseResponse> newList = new ArrayList<>();
+
+        for (Course c:unchanged) {
+            CourseResponse changed = new CourseResponse();
+
+            changed.set_id(c.get_id());
+            int indicator = c.getCategory();
+            if(indicator == 1)
+                changed.setCategory("JAVA");
+            else if(indicator == 2)
+                changed.setCategory("PEGA");
+            else
+                changed.setCategory("JS");
+            changed.setTitle(c.getTitle());
+            changed.setDescription(c.getDescription());
+            changed.setImg(c.getImg());
+            changed.setStatus(c.getStatus());
+
+            newList.add(changed);
+        }
+
+        return newList;
     }
 
 }
