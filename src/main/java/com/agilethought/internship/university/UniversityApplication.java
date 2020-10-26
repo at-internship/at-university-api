2c99e02a31aed68181c91e6855cdf983d5e2980c
VianeyG <vianey.garcia@agilethought.com>
2020-10-26 12:30:44 -0600

    PatternLayout in log4j2.properties

--- src/main/java/com/agilethought/internship/university/UniversityApplication.java
+++ src/main/java/com/agilethought/internship/university/UniversityApplication.java
@@ -1,27 +1,12 @@
 package com.agilethought.internship.university;
 
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-
-
 
 @SpringBootApplication
 public class UniversityApplication {
 
-	private static Logger logger = LogManager.getLogger(UniversityApplication.class);
-
 	public static void main(String[] args) {
-		logger.error("error log message!");
-		logger.trace ("trace log message!");
-		logger.debug ("Debug log message!");
-		logger.info ("info log message!");
-		logger.warn ("warn log message!");
-		logger.fatal ("fatal log message!");
-
 		SpringApplication.run(UniversityApplication.class, args);
 	}
 
