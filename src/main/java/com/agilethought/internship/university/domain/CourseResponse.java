fec463330ea24a2cb06ff506de3d927f63598f52
VianeyG <vianey.garcia@agilethought.com>
2020-11-11 10:45:50 -0600

    Update swagger

--- src/main/java/com/agilethought/internship/university/domain/CourseResponse.java
+++ src/main/java/com/agilethought/internship/university/domain/CourseResponse.java
@@ -1,5 +1,6 @@
 package com.agilethought.internship.university.domain;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
@@ -8,11 +9,17 @@ import org.springframework.data.annotation.Id;
 @Setter
 public class CourseResponse {
     @Id
+    @ApiModelProperty(value = "Course id", example = "5fabf9ee11993d0719574a76")
     public String _id;
+    @ApiModelProperty(value = "Course category", example = "JAVA")
     private String category;
+    @ApiModelProperty(value = "Course title", example = "Java 8 essentials")
     private String title;
+    @ApiModelProperty(value = "Course description", example = "Course about Java 8 basics")
     private String description;
+    @ApiModelProperty(value = "Logo of the Course", example = "img.jpg")
     private String img;
+    @ApiModelProperty(value = "Course status", example = "1")
     private int status;
 
 }
--- src/main/java/com/agilethought/internship/university/domain/UpdateCourseResponse.java
+++ src/main/java/com/agilethought/internship/university/domain/UpdateCourseResponse.java
@@ -1,5 +1,6 @@
 package com.agilethought.internship.university.domain;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -11,10 +12,15 @@ import lombok.ToString;
 @ToString
 public class UpdateCourseResponse {
 
+    @ApiModelProperty(value = "Course category", example = "JS")
     private String category;
+    @ApiModelProperty(value = "Course title", example = "JS essentials")
     private String title;
+    @ApiModelProperty(value = "Course description", example = "Course about JavaScript basics")
     private String description;
+    @ApiModelProperty(value = "Logo of the Course", example = "img.jpg")
     private String img;
+    @ApiModelProperty(value = "Course status", example = "2")
     private int status;
 
 
