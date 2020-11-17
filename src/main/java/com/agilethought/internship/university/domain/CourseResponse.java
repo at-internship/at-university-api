dda8bae08b94ebe42a692ab9455d23f62f163b4b
DanielJacoboN <psicodan234@gmail.com>
2020-11-17 13:21:20 -0600

    Lof4j2 and ToString settings added

--- src/main/java/com/agilethought/internship/university/domain/CourseResponse.java
+++ src/main/java/com/agilethought/internship/university/domain/CourseResponse.java
@@ -3,11 +3,14 @@ package com.agilethought.internship.university.domain;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 import org.springframework.data.annotation.Id;
 
 @Getter
 @Setter
+@ToString
 public class CourseResponse {
+
     @Id
     @ApiModelProperty(value = "Course id", example = "5fabf9ee11993d0719574a76")
     public String _id;
